7c4126463542802972822d2fda9bcd3683c782b7
Romil Wadhwani <romilwadhwani@gmail.com>
2026-09-13 18:17:30 +0530

    phase 5: add ThrottlerModule with global rate limiting via APP_GUARD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppConfigModule } from './config/config.module';
 import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [AppConfigModule, PrismaModule],
+  imports: [
+    AppConfigModule,
+    PrismaModule,
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        throttlers: [
+          {
+            ttl: configService.get<number>('THROTTLE_TTL', 60) * 1000,
+            limit: configService.get<number>('THROTTLE_LIMIT', 20),
+          },
+        ],
+      }),
+    }),
+  ],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
